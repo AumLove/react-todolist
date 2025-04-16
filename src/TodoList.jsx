5f2b91a542b86fa78dc469517551402629af6d47
ZS <824183803@qq.com>
2025-04-16 22:06:34 +0800

    feat :添加TODO功能

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,27 +1,54 @@
-import React from 'react';
-import { useState } from 'react';
-function TodoList(props) {
-
-    const [value, setValue] = useState("");
-
-    function clickHandle(){
-        props.store.dispatch(addListAction(value));
-        setValue("");
+import "./TodoList.css"
+import React, { useRef, useState } from 'react';
+export default function TODoList() {
+  const [toDoS, setArr] = useState(
+    ()=>{
+        const storedTodos = localStorage.getItem('toDoS');
+        return storedTodos ? JSON.parse(storedTodos) : [];
     }
+  );
+  const [inputValue, setInputValue] = useState('');
+  const listRef = useRef();
 
-    return (
-       <>
-        <ul>
-            <li>content：{props.content}</li>
-        </ul>
-       </>
-    );
+  const addHandle = () => {
+    const inputValue = listRef.current.value.trim();
+    if (inputValue) {
+      const newArr = [...toDoS];
+      newArr.push(inputValue);
+      setArr(newArr);
+      setInputValue('');
+      localStorage.setItem('toDoS', JSON.stringify(newArr));
+    }
+  };
+
+  const delHandle = (index) => {
+    const newArr = toDoS.filter((_, i) => i !== index);
+    setArr(newArr);
+  };
+
+  const onChangeInput = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <div class="toDoList">
+     <p>toDoS</p>
+    
+     <div>
+     <input placeholder='请输入' ref={listRef}  value={inputValue}
+       onChange={(e) => onChangeInput(e)}
+       />
+      <button onClick={addHandle}>增加</button>
+     </div>
+      <ul>
+        {toDoS.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => delHandle(index)}>删除</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-// 设置 props 的默认值
-TodoList.defaultProps = {
-    content : 100
-}
-
-
-export default TodoList;
